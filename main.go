e9407f2f8d785fc0b922832ccfcbe246fe215092
Jared Connor <jaredconnor301@gmail.com>
2023-02-28 05:08:42 +0000

    Updating description for clipping

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func main() {
 	// // Setup router
 	router.SetupRouter(app)
 
+	// Send a string back for GET calls to the endpoint "/"
+	app.Get("/", func(c *fiber.Ctx) error {
+		err := c.SendString("And the API is UP!")
+		return err
+	})
+
 	// Listen on Port
 	app.Listen(":3000")
 
